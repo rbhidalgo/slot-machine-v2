dcbe23e7b97eeac5f6505228b30237f23a72fc14
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-04-09 21:36:39 -0700

    add plays and winnings column

--- js/app.js
+++ js/app.js
@@ -31,16 +31,25 @@ let slotColumn7 = document.querySelector('.slot-column7')
 let slotColumn8 = document.querySelector('.slot-column8')
 let slotColumn9 = document.querySelector('.slot-column9')
 let betOnebtn = document.querySelector('.bet-one')
+let betThreebtn = document.querySelector('.bet-three')
+let betFivebtn = document.querySelector('.bet-five')
 let cubeAnimation = document.querySelector('.cube-animation')
 let beerCube = document.querySelector('.cube')
 let spinSoundEff = document.querySelector('.spin-sound')
 let winSoundEff = document.querySelector('.win-sound')
+let bet3Icons = document.querySelector('.bet3')
+let bet5Icons = document.querySelector('.bet5')
+let totalPointsDisplay = document.querySelector('.total')
+let lastWinDisplay = document.querySelector('.last-win')
+let lastBetDisplay = document.querySelector('.last-bet-made')
 var currency = 0;
+var totalPoints = 0;
 
 
 twentyBtn.addEventListener('click', function() {
     console.log(`twenty was pressed!`)
     currency = 20;
+    totalPointsDisplay.innerHTML = `Total ${currency}`;
     console.log(currency)
     enterAmount.style.display = 'none';
     containerDiv.style.display = 'inline';
@@ -49,6 +58,7 @@ twentyBtn.addEventListener('click', function() {
 fiftyBtn.addEventListener('click', function() {
     console.log(`fifty was pressed!`)
     currency = 50;
+    totalPointsDisplay.innerHTML = currency;
     console.log(currency)
     enterAmount.style.display = 'none';
     containerDiv.style.display = 'inline'; 
@@ -57,14 +67,49 @@ fiftyBtn.addEventListener('click', function() {
 hundBtn.addEventListener('click', function() {
     console.log(`hundred was pressed!`)
     currency = 100;
+    totalPointsDisplay.innerHTML = currency;
     console.log(currency)
     enterAmount.style.display = 'none';
     containerDiv.style.display = 'inline';
 } )
 
 betOnebtn.addEventListener('click', function(){
-    resetAnimation()
     resetColor()
+    bet3Icons.style.display = 'none';
+    bet5Icons.style.display = 'none';
+    // winSoundEff.pause()
+    console.log(`bet one was pressed`)
+    spinSoundEff.play()
+    setInterval (() => { 
+    betOnebtn.style.animation = 'background-animation 3s 0s 1';
+    slotColumn1.style.animation = 'cube-slot-ani 1.3s 0s 2';
+    slotColumn2.style.animation = 'cube-slot-ani 1.2s 0s 2';
+    slotColumn3.style.animation = 'cube-slot-ani 1.3s 0s 2';
+    slotColumn4.style.animation = 'cube-slot-ani 1.2s 0s 2';
+    slotColumn5.style.animation = 'cube-slot-ani 1s 0s 2';
+    slotColumn6.style.animation = 'cube-slot-ani .9s 0s 2';
+    slotColumn7.style.animation = 'cube-slot-ani .8s 0s 2';
+    slotColumn8.style.animation = 'cube-slot-ani .7s 0s 2';
+    slotColumn9.style.animation = 'cube-slot-ani .6s 0s 2';
+    }, 100);
+    photoGenerate()
+    resetAnimation()
+    setTimeout(() => {
+    totalPointsDisplay.innerHTML = `Total ${currency}`;
+    lastWinDisplay.innerHTML = `Last Win ${totalPoints}`;
+    totalPointsDisplay.style.color = 'red';
+    lastWinDisplay.style.color = 'green';
+    }, 3000);
+    setTimeout(() => {
+    lastWinDisplay.style.color = 'black';
+    totalPointsDisplay.style.color = 'black';
+    }, 5000);
+})
+
+betThreebtn.addEventListener('click', function(){
+    resetColor()
+    bet3Icons.style.display = 'flex';
+    bet5Icons.style.display = 'none';
     // winSoundEff.pause()
     console.log(`bet one was pressed`)
     spinSoundEff.play()
@@ -80,21 +125,58 @@ betOnebtn.addEventListener('click', function(){
     slotColumn9.style.animation = 'cube-slot-ani .6s 0s 2';
     }, 100);
     photoGenerate()
+    resetAnimation()
 })
 
+betFivebtn.addEventListener('click', function(){
+    resetColor()
+    bet3Icons.style.display = 'flex';
+    bet5Icons.style.display = 'flex';
+    // winSoundEff.pause()
+    console.log(`bet one was pressed`)
+    spinSoundEff.play()
+    setInterval (() => { 
+    slotColumn1.style.animation = 'cube-slot-ani 1.3s 0s 2';
+    slotColumn2.style.animation = 'cube-slot-ani 1.2s 0s 2';
+    slotColumn3.style.animation = 'cube-slot-ani 1.3s 0s 2';
+    slotColumn4.style.animation = 'cube-slot-ani 1.2s 0s 2';
+    slotColumn5.style.animation = 'cube-slot-ani 1s 0s 2';
+    slotColumn6.style.animation = 'cube-slot-ani .9s 0s 2';
+    slotColumn7.style.animation = 'cube-slot-ani .8s 0s 2';
+    slotColumn8.style.animation = 'cube-slot-ani .7s 0s 2';
+    slotColumn9.style.animation = 'cube-slot-ani .6s 0s 2';
+    }, 100);
+    photoGenerate()
+    resetAnimation()
+})
+// let slotcol1anim = slotColumn1.style.animation = 'cube-slot-ani 1.3s 0s 2';
+// let slotcol2anim = slotColumn2.style.animation = 'cube-slot-ani 1.2s 0s 2';
+// let slotcol3anim = slotColumn3.style.animation = 'cube-slot-ani 1.3s 0s 2';
+// let slotcol4anim = slotColumn4.style.animation = 'cube-slot-ani 1.2s 0s 2';
+// let slotcol5anim = slotColumn5.style.animation = 'cube-slot-ani 1s 0s 2';
+// let slotcol6anim = slotColumn6.style.animation = 'cube-slot-ani .9s 0s 2';
+// let slotcol7anim = slotColumn7.style.animation = 'cube-slot-ani .8s 0s 2';
+// let slotcol8anim = slotColumn8.style.animation = 'cube-slot-ani .7s 0s 2';
+// let slotcol9anim = slotColumn9.style.animation = 'cube-slot-ani .6s 0s 2';
+
+// function aniOn () {
+//     slotcol1anim.
+// }
+
 function resetColor () {
-    slotColumn1.style.backgroundColor = '#7c05f2';
-    slotColumn2.style.backgroundColor = '#7c05f2';
-    slotColumn3.style.backgroundColor = '#7c05f2';
-    slotColumn4.style.backgroundColor = '#7c05f2';
-    slotColumn5.style.backgroundColor = '#7c05f2';
-    slotColumn6.style.backgroundColor = '#7c05f2';
-    slotColumn7.style.backgroundColor = '#7c05f2';
-    slotColumn8.style.backgroundColor = '#7c05f2';
-    slotColumn9.style.backgroundColor = '#7c05f2';
+    slotColumn1.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn2.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn3.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn4.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn5.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn6.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn7.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn8.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
+    slotColumn9.style.backgroundColor = 'rgba(189, 191, 193, 0.8)';
 }
 
 function resetAnimation () {
+    betOnebtn.style.animation = '';
     slotColumn1.style.animation = '';
     slotColumn2.style.animation = '';
     slotColumn3.style.animation = '';
@@ -141,9 +223,6 @@ function photoGenerate() {
     }
 
 function colCheckBkc1() {
-    row2Column2Photo.src = beerSlot2Photos[randNumR2C2]
-    row1Column3Photo.src = beerSlot2Photos[randNumR1C3]
-    row3Column1Photo.src = beerSlot2Photos[randNumR3C1]
     slotColumn4.style.backgroundColor = '#0766ef';
     slotColumn1.style.backgroundColor = '#0766ef';
     slotColumn7.style.backgroundColor = '#0766ef';
@@ -220,6 +299,7 @@ function columnCheck () {
         row1Column1Photo.src = beerSlot2Photos[randNumR1C1]
         row3Column1Photo.src = beerSlot2Photos[randNumR3C1]
         currency += 1
+        totalPoints +=1
         colCheckBkc1()
         console.log(`you won 1 points - ${currency}`)
     }
@@ -228,6 +308,7 @@ function columnCheck () {
         row1Column2Photo.src = beerSlot2Photos[randNumR1C2]
         row3Column2Photo.src = beerSlot2Photos[randNumR3C2]
         currency += 1
+        totalPoints +=1
         colCheckBkc2()
             console.log(`you won 1 points - ${currency}`)
         }
@@ -236,6 +317,7 @@ function columnCheck () {
         row1Column3Photo.src = beerSlot2Photos[randNumR1C3]
         row3Column3Photo.src = beerSlot2Photos[randNumR3C3]
         currency += 1
+        totalPoints +=1
         colCheckBkc3()
         console.log(`you won 1 points - ${currency}`)
         }
@@ -249,6 +331,7 @@ function rowCheck () {
         row1Column1Photo.src = beerSlot2Photos[randNumR1C1]
         row1Column3Photo.src = beerSlot2Photos[randNumR1C3]
         currency += 1
+        totalPoints += 1
         rowCheckBkc1()
         console.log(`you won 1 points - ${currency}`)
     }
@@ -257,6 +340,7 @@ function rowCheck () {
         row2Column1Photo.src = beerSlot2Photos[randNumR2C1]
         row2Column3Photo.src = beerSlot2Photos[randNumR2C3]
         currency += 1
+        totalPoints += 1
         rowCheckBkc2()
         console.log(`you won 1 points - ${currency}`)
     }
@@ -265,6 +349,7 @@ function rowCheck () {
         row3Column1Photo.src = beerSlot2Photos[randNumR3C1]
         row3Column3Photo.src = beerSlot2Photos[randNumR3C3]
         currency += 1
+        totalPoints += 1
         rowCheckBkc3()
         console.log(`you won 1 points - ${currency}`)
     }
@@ -275,6 +360,7 @@ function rowCheck () {
 function crossCheck () {
     if (randNumR2C2 == randNumR1C3 && randNumR2C2 == randNumR3C1) {
         currency += 1
+        totalPoints += 1
         crossCheckBkc1()
         console.log(`you won 1 point - ${currency}`) 
     }
@@ -283,6 +369,7 @@ function crossCheck () {
         row3Column3Photo.src = beerSlot2Photos[randNumR3C3]
         row1Column1Photo.src = beerSlot2Photos[randNumR1C1]
         currency += 1
+        totalPoints += 1
         crossCheckBkc2()
         console.log(`you won 1 point - ${currency}`) 
         }
@@ -303,6 +390,7 @@ function crossCheck () {
             row1Column3Photo.src = beerSlot2Photos[randNumR1C3]
             row2Column3Photo.src = beerSlot2Photos[randNumR2C3]
             currency += 50
+            totalPoints += 50
             squareCheckBkc()
             console.log(`YOU WON 50 POINTS!!!! - ${currency}`)
             }
@@ -323,6 +411,8 @@ function crossCheck () {
             row3Column2Photo.src = beerSlot2Photos[randNumR3C2]
             row3Column3Photo.src = beerSlot2Photos[randNumR3C3]
             currency += 1000000
+            totalPoints += 1000000
+            jackpotCheckBkc()
             console.log(`JACKPOT!!!!!!!! ${currency}`)
             }
         else currency -= 1
@@ -340,7 +430,8 @@ function gameOver () {
     // enterAmount.style.animation = 'fadein, 10s, linear, 10s, 1, normal';
     enterAmount.style.animationName = 'fadein';
     enterAmount.style.display = 'flex';
-    document.querySelector('.amount-h1').innerHTML = 'Play Again?';``
+    document.querySelector('.amount-h1').innerHTML = 'Play Again?';
+    currency = 0;
     // const textAfterH1 = document.createElement('H1');
     // textAfterH1.innerHTML = "You deserve a beer!";
     // enterAmount.appendChild(textAfterH1);
